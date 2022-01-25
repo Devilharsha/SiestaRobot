d384a8f69de697fa8a7a1082c45535fd79f2f1c2
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-01-25 06:16:49 +0530

    Update alive.py

--- SiestaRobot/modules/alive.py
+++ SiestaRobot/modules/alive.py
@@ -9,16 +9,16 @@ from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/d005e24a2a5684446df6c.jpg"
+PHOTO = "http://telegra.ph/file/4f5ba37d96a552dd6d814.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm seto Kaiba Robot.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ninja Naruto Robot.** \n\n"
   TEXT += "💠 **I'm Working Properly** \n\n"
   TEXT += f"💠 **My Master : [harsha](https://t.me/harshahero)** \n\n"
   TEXT += f"💠 **Library Version :** `{telever}` \n\n"
   TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/SetoKaibaRobot?start=help"), Button.url("Support", "https://t.me/sctbotsSupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Ninja_naruto_Robott?start=help"), Button.url("Support", "https://t.me/sctbotsSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
